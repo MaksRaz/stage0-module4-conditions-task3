518d733a5c60189e7808d41581358038faa9f234
Maxim Razinkov <vfx60@mail.ru>
2022-08-08 02:18:19 +0300

    commit

--- src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task3;
 
 public class AliquotNumbers {
     public void isFirstAliquot(int first, int second) {
+        if (Math.sqrt(second) == first){
+            System.out.println("Aliquot");
+        }
+        else {
+            System.out.println("Not aliquot");
+        }
     }
 }
